a2d7dee42e01e872fe48fc51c57ceae29d6ab4b8
Benjamin Atbi <benjaminatbi@gmail.com>
2026-01-26 18:09:28 -0800

    Updated Patches to current game patch, reorganized, and updated Readme

--- BatbiWrathQOL/Patch.cs
+++ BatbiWrathQOL/Patch.cs
@@ -169,7 +169,9 @@ namespace BatbiWrathQOL
     }
 
 
-    //
+    //**********************************
+    // Blueprint Patches 
+    //**********************************
     [HarmonyPatch(typeof(BlueprintsCache))]
     public class BlueprintsCache_Patches
     {
@@ -178,31 +180,35 @@ namespace BatbiWrathQOL
 
         static void Postfix()
         {
-            HolyBombFix();
+            //HolyBombFix();
             LichSkinPatch();
             //SkinUnlockerPatch();
         }
 
+        //**********************************
+        // Alchemist Holy Bomb Bugfix (Officially Patched)
+        //**********************************
+        //public static void HolyBombFix()
+        //{
+        //    try
+        //    {
+        //        var HolyBomb = ResourcesLibrary.TryGetBlueprint<BlueprintAbility>("b94ee802dc1574b4fb71215a4a6f11dc");
+        //        var mainTargetConditional = HolyBomb.Components.OfType<AbilityEffectRunAction>().First().Actions.Actions.OfType<Conditional>().First();
+        //        var evilConditional = mainTargetConditional.IfTrue.Actions.OfType<Conditional>().First().IfFalse.Actions.OfType<Conditional>().First();
+        //        var reanimatorConditional = evilConditional.IfTrue.Actions.OfType<Conditional>().First();
+        //        reanimatorConditional.IfTrue.Actions.OfType<ContextActionDealDamage>().First().Half = false;
+        //        reanimatorConditional.IfFalse.Actions.OfType<ContextActionDealDamage>().First().Half = false;
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        Main.DebugError(e);
+        //    }
+        //}
 
-        public static void HolyBombFix()
-        {
-            try
-            {
-                var HolyBomb = ResourcesLibrary.TryGetBlueprint<BlueprintAbility>("b94ee802dc1574b4fb71215a4a6f11dc");
-                var mainTargetConditional = HolyBomb.Components.OfType<AbilityEffectRunAction>().First().Actions.Actions.OfType<Conditional>().First();
-                var evilConditional = mainTargetConditional.IfTrue.Actions.OfType<Conditional>().First().IfFalse.Actions.OfType<Conditional>().First();
-                var reanimatorConditional = evilConditional.IfTrue.Actions.OfType<Conditional>().First();
-                reanimatorConditional.IfTrue.Actions.OfType<ContextActionDealDamage>().First().Half = false;
-                reanimatorConditional.IfFalse.Actions.OfType<ContextActionDealDamage>().First().Half = false;
-            }
-            catch (Exception e)
-            {
-                Main.DebugError(e);
-            }
-        }
 
-
-
+        //**********************************
+        // Unlock Skin options for different races (WIP/Dropped)
+        //**********************************
         class RampNameCompare : IEqualityComparer<Texture2D>
         {
             public bool Equals(Texture2D a, Texture2D b)
@@ -280,66 +286,17 @@ namespace BatbiWrathQOL
             }
         }
 
+        //**********************************
+        // Disable Lich Skin 
+        //**********************************
         public static void LichSkinPatch()
         {
             try
             {
-                //var LichDesaturation = ResourcesLibrary.TryGetBlueprint<KingmakerEquipmentEntity>("a4356509220f4bf19f05f70eb5db9240");
-                //Main.DebugLog($"loaded lich {(LichDesaturation == null ? "null" : LichDesaturation.GetType().ToString())}");
-                var DhampirRace = ResourcesLibrary.TryGetBlueprint<BlueprintRace>("64e8b7d5f1ae91d45bbf1e56a3fdff01");
-                //Main.DebugLog($"loaded Dhampir {(DhampirRace == null ? "null" : DhampirRace.GetType().ToString())}");
-
-                //Main.DebugLog($"ramps {String.Join(",",DhampirRace.FemaleOptions.Heads.First().Load().PrimaryRamps)}");
-                //var DhampirRamps = DhampirRace.MaleOptions.Heads.First().Load().PrimaryRamps;
-
-                //var Lich = LichDesaturation.m_FemaleArray.First().Load();
-
-                //foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(Lich))
-                //{
-                //    string name = descriptor.Name;
-                //    object value = descriptor.GetValue(Lich);
-                //    Main.DebugLog($"{name}={value}");
-                //}
-
-                //LichDesaturation.m_FemaleArray.First().Load().OutfitParts = null;
-                //LichDesaturation.m_MaleArray.First().Load().OutfitParts = null;
-
-                //Main.DebugLog($"name: {Lich.name},layer: {Lich.Layer},body parts: {Lich.BodyParts.Count},outfitparts: {Lich.OutfitParts.Count}, Bakedtextures: {Lich.BakedTextures}," +
-                //    $"Can't be hidden: {Lich.CantBeHiddenByDollRoom}, color presets: {Lich.ColorPresets}, primary count: {Lich.m_PrimaryRamps.Count}, " +
-                //    $"Secondary count: {Lich.m_SecondaryRamps.Count}, special prim count: {Lich.m_SpecialPrimaryRamps.Count}, special sec count: {Lich.m_SpecialSecondaryRamps.Count}," +
-                //    $"prim color profile:{Lich.PrimaryColorsProfile}, sec color profile:{Lich.SecondaryColorsProfile},");
-
-                //Main.DebugLog($"lich: {LichEE.name}, {LichEE.m_PrimaryRamps.Count}, {LichEE.m_SecondaryRamps.Count}, {LichEE.m_SpecialPrimaryRamps.Count}, {LichEE.m_SpecialSecondaryRamps.Count}");
-                //LichDesaturation.m_FemaleArray.First().Load().PrimaryColorsProfile.Ramps = DhampirRamps;
-                //LichDesaturation.m_MaleArray.First().Load().PrimaryColorsProfile.Ramps = DhampirRamps;
-
-
                 var LichVisuals = ResourcesLibrary.TryGetBlueprint<BlueprintClassAdditionalVisualSettings>("6a60a86905b24137ae1e4300b6ab0841");
 
                 LichVisuals.CommonSettings.m_EquipmentEntities = new KingmakerEquipmentEntityReference[] { };
                 LichVisuals.ColorRamps = new BlueprintClassAdditionalVisualSettings.ColorRamp[] { };
-
-                var skinTypeCodes = new HashSet<long>()
-                {
-                    256L, 512L, 2048L,
-                };
-
-                //LichVisuals.ColorRamps = LichVisuals.ColorRamps.Where(c => skinTypeCodes.Contains(c.m_Type)).ToArray();
-
-                //Main.DebugLog($"lich ramps: {String.Join(" ", LichVisuals.ColorRamps.Select(c => c.m_Type))}");
-                //;
-                //foreach (var colorramp in LichVisuals.ColorRamps.Where(c => !skinTypeCodes.Contains(c.m_Type)))
-                //{
-                //    colorramp.m_Primary = setrampval(colorramp.Primary);
-                //    colorramp.m_Secondary = setrampval(colorramp.Secondary);
-                //    colorramp.m_SpecialPrimary = setrampval(colorramp.SpecialPrimary);
-                //    colorramp.m_SpecialSecondary = setrampval(colorramp.SpecialSecondary);
-                //}
-
-                //var LichVisuals2 = ResourcesLibrary.TryGetBlueprint<BlueprintClassAdditionalVisualSettings>("6a60a86905b24137ae1e4300b6ab0841");
-
-
-
             }
             catch (Exception e)
             {
--- README.md
+++ README.md
@@ -1,7 +1,14 @@
-# Kingmaker UI changes
+# Batbi Wrath QOL
+
+### Features
 
 #### Remove Outline
-when you activate highlight view, your party will not be surrounded by a green outline
+When you activate highlight view, your party members will not be surrounded by a green outline. This is primarily intended to work in concert with the "toggle highlight" option in [Toybox](https://github.com/xADDBx/ToyBox-Wrath), so you don't have an green outline around your characters at all times.
 
 #### Remember Spell Levels
-when you select a character, the spellbook popup will default to the level it was at the last time you selected that character, rather than reverting to cantrips every time.
+When you open the spellbook popup on the hotbar, it will remember the spell lvl page it was previously open to for that character. In the base game, whenever you close the popup or switch characters, it jumps back to the cantrip page.
+
+#### Disable Lich Skin
+Removes the ashen skin effect of the lich path. Requires full game reload to take effect.
+
+If you're a human and still want to look somewhat pallid, you can use [Visual Adjustments 2](https://github.com/BarleyFlour/VisualAdjustments2) to change your racial appearance to Dhampir without changing your actual race.
