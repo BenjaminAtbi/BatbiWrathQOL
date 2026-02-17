e44d685d1e654fb6875cae36ce53db8b50e58304
Benjamin Atbi <benjaminatbi@gmail.com>
2026-02-16 22:39:03 -0800

    Added Summon ability toggles default off

---
+++ BatbiWrathQOL/src/Patches/SummonToggles.cs
@@ -0,0 +1,51 @@
+﻿using HarmonyLib;
+using Kingmaker.UI.MVVM._VM.ActionBar;
+using Kingmaker.UnitLogic.Mechanics.Actions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BatbiWrathQOL.src.Patches
+{
+    internal class SummonToggles
+    {
+        
+        /*
+         * When spawning a summoned creature, initially toggle off combat abilities 
+         */
+        [HarmonyPatch(typeof(ContextActionSpawnMonster), nameof(ContextActionSpawnMonster.RunAction))]
+        static class SummonsAbilitiesToggleOff
+        {
+            static void Prefix(ContextActionSpawnMonster __instance)
+            {
+                if (!Main.Enabled) return;
+                __instance.AfterSpawn.Actions = __instance.AfterSpawn.Actions.Concat(new[] { new ContextActionToggleAbilities() }).ToArray();
+            }
+        }
+
+        internal class ContextActionToggleAbilities : ContextAction
+        {
+            public override string GetCaption()
+            {
+                return "Initially toggle off combat abilities with To-hit drawbacks";
+            }
+
+            public override void RunAction()
+            {
+                if (Target.Unit.IsSummoned())
+                {
+                    foreach(var ability in Target.Unit.ActivatableAbilities){
+                        if(ability.Blueprint.AssetGuidThreadSafe == "a7b339e4f6ff93a4697df5d7a87ff619" //power attack
+                            || ability.Blueprint.AssetGuidThreadSafe == "94ed44fc6c8a717489eebdf8b364d4d8" //piranha strike
+                            || ability.Blueprint.AssetGuidThreadSafe == "ccde5ab6edb84f346a74c17ea3e3a70c") //deadly aim
+                        {
+                            ability.TurnOffImmediately();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
